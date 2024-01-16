98265514e6a41e091c0e82f3c441ebbc5d68aa52
traviho <thdarkblue@hotmail.com>
2024-01-16 12:23:12 -0500

    complete tvmaze functions

--- apis-tvmaze/tvmaze.js
+++ apis-tvmaze/tvmaze.js
@@ -3,6 +3,7 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const $episodesList = $("#episodesList");
 
 
 /** Given a search term, search for tv shows that match that query.
@@ -12,27 +13,13 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-           normal lives, modestly setting aside the part they played in
-           producing crucial intelligence, which helped the Allies to victory
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-        "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ];
+  return fetch("https://api.tvmaze.com/search/shows?q=" + term).then(
+    res => res.json()
+  ).then(data => (
+    data.map(obj => obj['show'])
+  ));
 }
 
 
@@ -40,14 +27,14 @@ async function getShowsByTerm( /* term */) {
 
 function populateShows(shows) {
   $showsList.empty();
-
+  console.log("Populating shows...")
   for (let show of shows) {
     const $show = $(
       `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
-              alt="Bletchly Circle San Francisco"
+              src="${show.image.original ?? "https://tinyurl.com/tv-missing"}"
+              alt="Movie Image"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -61,6 +48,14 @@ function populateShows(shows) {
       `);
 
     $showsList.append($show);
+    const $episodeButtons = $(".Show-getEpisodes");
+    $episodeButtons.unbind().on("click", async function() {
+      const showID = $(this).parent().parent().parent().attr('data-show-id');
+      const episodes = await getEpisodesOfShow(showID);
+      console.log(showID);
+      console.log(episodes);
+      populateEpisodes(episodes);
+    })
   }
 }
 
@@ -87,8 +82,20 @@ $searchForm.on("submit", async function (evt) {
  *      { id, name, season, number }
  */
 
-// async function getEpisodesOfShow(id) { }
+async function getEpisodesOfShow(id) {
+  return fetch(`https://api.tvmaze.com/shows/${id}/episodes`).then(res => (
+    res.json()
+  )).then(data => (
+    data
+  ));
+}
 
-/** Write a clear docstring for this function... */
+/** Given list of episodes, create markup for each and DOM */
 
-// function populateEpisodes(episodes) { }
+function populateEpisodes(episodes) {
+  $episodesArea.show();
+  $episodesList.empty();
+  for (let episode of episodes) {
+    $episodesList.append(`<li>${episode.name} (season ${episode.season}, number ${episode.number})</li>`);
+  }
+}
