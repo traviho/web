0aea22fb2b62ca8b7ebcb752a5163727a4f3636f
traviho <thdarkblue@hotmail.com>
2024-01-16 13:36:37 -0500

    giphy party form

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,19 @@
-console.log("Let's get this party started!");
+$searchForm = $("#search-form");
+$searchInput = $("#search-input");
+$gifContainer = $("#gif-container");
+$removeButton = $('#remove-button');
+
+$searchForm.on('submit', function(event) {
+    event.preventDefault();
+    const text = $searchInput.val();
+    console.log(text);
+    $searchInput.val("");
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${text}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`).then(res => {
+        const firstResult = res.data.data[0];
+        $gifContainer.append(`<img class="gif-img" src="${firstResult.images.original.url}" />`)
+    });
+})
+
+$removeButton.on('click', function(event) {
+    $gifContainer.html('');
+});
